ed5cf8d4ad90e58a820a97d0cb585b18a86274e6
Gregg Nobga <greggnobga@gmail.com>
2022-05-07 21:10:44 -0500

    Run webpack in development mode.

--- app/Http/Controllers/Dashboard/Crypto/ScreenController.php
+++ app/Http/Controllers/Dashboard/Crypto/ScreenController.php
@@ -6,8 +6,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 
-use Illuminate\Support\Facades\View;
-
 class ScreenController extends Controller {
     /**
      * Display a listing of the resource.
